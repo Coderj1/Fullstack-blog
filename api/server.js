e7935a368c9fe4e8482ab11af6ed5e8163873636
unknown <ramselove52@gmail.com>
2024-04-30 09:02:14 +0100

    first commit

---
+++ api/server.js
@@ -0,0 +1,53 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import cookieParser from "cookie-parser";
+import dotenv from 'dotenv'
+import PostRoutes from "./routes/post.route.js";
+import CommentRoutes from './routes/comment.route.js'
+import path from 'path'
+
+    const __dirname = path.resolve()
+
+    dotenv.config();
+
+mongoose.connect('mongodb://127.0.0.1:27017/Fullstack', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(
+    () => {
+        console.log('You are now connected...!!!');
+    }).catch((e) => {
+        console.log(e)
+    });
+
+    const app = express();
+
+        app.use(express.json());
+        app.use(cookieParser())
+
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
+});
+
+app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/post', PostRoutes)
+app.use('/api/comment', CommentRoutes)
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join((__dirname, 'client', 'dist', 'index.html')))
+})
+
+app.use(express.static(path.join__dirname, 'client/dist'))
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({ 
+        success: false,
+        statusCode,
+        message 
+        });
+})
